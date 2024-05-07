3dea7261ed6be571add13e8362d0d78948d2b6d4
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-05-07 17:35:57 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,3 +12,18 @@ function App() {
 }
 
 export default App;
+import CardForm from './components/CardForm.jsx'; // Import the CardForm component
+import PropertyCardList from './Components/PropertyCardList.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Property Details</h1>
+      <CardForm />
+      <PropertyCardList/>
+    </div>
+  );
+}
+
+export default App;
+
