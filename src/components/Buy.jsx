50c38b5c01395fd86166885b5ffed9b67b5daf88
Daniel <dani3lkamanthi@gmail.com>
2024-05-08 12:40:16 +0300

    Adds routing functionality

---
+++ src/components/Buy.jsx
@@ -0,0 +1,10 @@
+import PropertyCardList from "./PropertyCardList";
+function Buy() {
+    return (
+        <div>
+            <h2>The following houses are available for purchase</h2>
+            <PropertyCardList/>
+        </div>
+    )
+}
+export default Buy;
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to this Real Estate Listings App</h1>
+        <p>What were you looking to do?</p>
+        <nav>
+          <Link to="/buy" className="nav-link">Buy</Link>
+          <Link to="/sell" className="nav-link">Sell</Link>
+          <Link to="/rent" className="nav-link">Rent</Link>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
