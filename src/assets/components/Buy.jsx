3b3a1a936e0458058e936f8a95d43d6a8f5c1716
Daniel <dani3lkamanthi@gmail.com>
2024-05-08 10:57:05 +0300

    Add new components and main.jsx

---
+++ src/assets/components/Buy.jsx
@@ -0,0 +1,8 @@
+function Buy() {
+    return (
+        <div>
+            <h2>The following houses are available for purchase</h2>
+        </div>
+    )
+}
+export default Buy;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './assets/components/App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
