aab58df6804b246ac88853782f16cb2843a60a58
Daniel <dani3lkamanthi@gmail.com>
2024-05-08 11:46:36 +0300

    Adds files

---
+++ src/components/Sell.jsx
@@ -0,0 +1,10 @@
+import CardForm from "./CardForm";
+function Sell() {
+    return (
+        <div>
+            <h2>Please enter the details of the house you want to sell</h2>
+            <CardForm />
+        </div>
+    )
+}
+export default Sell;
