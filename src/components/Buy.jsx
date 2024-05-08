a2edb32cdf357d7a8cc4666cd12638fe7d681f14
Daniel <dani3lkamanthi@gmail.com>
2024-05-08 10:58:33 +0300

    Moves files

--- src/components/Buy.jsx
+++
@@ -1,8 +0,0 @@
-function Buy() {
-    return (
-        <div>
-            <h2>The following houses are available for purchase</h2>
-        </div>
-    )
-}
-export default Buy;
--- src/components/Sell.jsx
+++
@@ -1,8 +0,0 @@
-function Sell() {
-    return (
-        <div>
-            <h2>Please enter the details of the house you want to sell</h2>
-        </div>
-    )
-}
-export default Sell;
